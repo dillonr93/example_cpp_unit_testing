d3700c6917d2d5e46de8e8179027d6401f7fd252
dillonramadhar <dillonramadhar93@hotmail.com>
2023-06-14 14:43:28 -0400

    Added network effects

--- main/testable_code.cpp
+++ main/testable_code.cpp
@@ -1,4 +1,6 @@
+#include <cmath>
 #include <gtest/gtest.h>
+#include <list>
 #include "consoledateinputter.h"
 #include "absl/time/time.h"
 #include "absl/time/clock.h"
@@ -74,5 +76,76 @@ TEST(DATECOMPARE, DATE){
   int ageCounter = age(nowDate.year(), nowDate.month(), nowDate.day(), 1993, 8, 1);
 
   std::cout << ageCounter << std::endl;
+
+}
+
+struct User{};
+struct Network{};
+
+/// Direct network effects occur when users can communicate with each other. More users increase the value of the network.
+/// More users increases direct value to other users.
+struct DirectNetwork : public Network{
+
+  DirectNetwork(std::list<User> users): users(users){}
+  
+  int size(){
+    return users.size();
+  }
+
+  void communicate(User a, User b){
+  }
+
+  std::list<User> users;
+};
+
+
+/// Complementary goods/service increases as the number of users increase.
+struct IndirectNetwork{
+
+};
+
+struct NetworkValueCalculator{
+
+  double valueOfNetwork(int networkSize){
+    return std::pow(networkSize, 2);
+  }
+
+};
+
+
+struct Distribution{
+
+  void leverageExistingNetwork(Network network){
+    std::cout << "Using other networks" << std::endl;
+  }  
+
+  void virality(User a){
+    std::cout << "User invites and brings other users. May provide incentive for users." << std::endl;
+  }
+
+};
+
+struct GrossMargins{
+  
+};
+
+struct OperationalScalability{
+
+};
+
+TEST(DIRECT,NETWORK_EFFECT){
+
+  User a,b,c;
+  DirectNetwork network({a,b,c});
+
+  EXPECT_EQ(network.size(),3);
+
+  NetworkValueCalculator calculator;
+
+  EXPECT_EQ(calculator.valueOfNetwork(network.size()),9);
+
+  Distribution distribution;
+  distribution.leverageExistingNetwork(Network());
+  distribution.virality(User());
 
 }
